c3ed61f9e08b46e3a96c8ba7a259b4d47ef6290e
Marylia Nieves <marylian@fullsail.edu>
2012-09-24 23:03:53 -0400

    Added the files and folders to the repository

---
+++ js/json.js
@@ -0,0 +1,34 @@
+//Marylia Nieves
+// VFW August 23, 2012
+//JSON File 
+
+
+var json = {
+	"task1": {
+		"checkbox": ["Choose a day", "Monday"],
+		"sub": ["Subject:", "Science"],
+		"period": ["Period:", "1"],
+		"grade": ["Grade Level:", "1"],
+		"category": ["Categories:","Grade"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "Teacher Tasks"]
+	},
+	"task2": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "2"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "To do"]
+	},
+		"task3": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Reading"],
+		"period": ["Period:", "3"],
+		"grade": ["Grade Level:", "3"],
+		"category": ["Categories:","Projects"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "Teacher Tasks"]
+	}
+}
