8dfc4d35955e4a863cbcb2024579837bbb8c5509
marylian <marylian@fullsail.edu>
2012-09-27 09:24:08 -0300

    Updated json file with 20 dummy data items.

--- js/json.js
+++ js/json.js
@@ -1,5 +1,5 @@
-//Marylia Nieves
-// VFW August 23, 2012
+	//Marylia Nieves
+// MiU September 27, 2012
 //JSON File 
 
 
@@ -22,7 +22,7 @@ var json = {
 		"date": ["Due Date:", "2012-08-08"],
 		"comments": ["My Notes:", "To do"]
 	},
-		"task3": {
+	"task3": {
 		"checkbox": ["Choose a day", "Wednesday"],
 		"sub": ["Subject:", "Reading"],
 		"period": ["Period:", "3"],
@@ -30,5 +30,158 @@ var json = {
 		"category": ["Categories:","Projects"],
 		"date": ["Due Date:", "2012-08-08"],
 		"comments": ["My Notes:", "Teacher Tasks"]
-	}
+	},
+	"task4": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "2"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "To do"]
+	},
+	"task5": {
+		"checkbox": ["Choose a day", "Monday"],
+		"sub": ["Subject:", "Reading"],
+		"period": ["Period:", "1"],
+		"grade": ["Grade Level:", "1"],
+		"category": ["Categories:","Contact"],
+		"date": ["Due Date:", "2012-09-24"],
+		"comments": ["My Notes:", "To do"]	
+	},	
+	"task6": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Social Studies"],
+		"period": ["Period:", "2"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Meetings"],
+		"date": ["Due Date:", "2012-09-25"],
+		"comments": ["My Notes:", "To do"]	
+	},		
+	"task7": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Science"],
+		"period": ["Period:", "3"],
+		"grade": ["Grade Level:", "3"],
+		"category": ["Categories:","Tests"],
+		"date": ["Due Date:", "2012-09-26"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task8": {
+		"checkbox": ["Choose a day", "Thursday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "4"],
+		"grade": ["Grade Level:", "4"],
+		"category": ["Categories:","Grade"],
+		"date": ["Due Date:", "2012-09-27"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task9": {
+		"checkbox": ["Choose a day", "Friday"],
+		"sub": ["Subject:", "Writing"],
+		"period": ["Period:", "5"],
+		"grade": ["Grade Level:", "5"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-09-28"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task10": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Science"],
+		"period": ["Period:", "1"],
+		"grade": ["Grade Level:", "1"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-09-29"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task11": {
+		"checkbox": ["Choose a day", "Thursday"],
+		"sub": ["Subject:", "Writing"],
+		"period": ["Period:", "4"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-09-30"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task12": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "2"],
+		"grade": ["Grade Level:", "3"],
+		"category": ["Categories:","Contact"],
+		"date": ["Due Date:", "2012-10-01"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task13": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "4"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-10-02"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task14": {
+		"checkbox": ["Choose a day", "Thursday"],
+		"sub": ["Subject:", "Reading"],
+		"period": ["Period:", "5"],
+		"grade": ["Grade Level:", "1"],
+		"category": ["Categories:","Grade"],
+		"date": ["Due Date:", "2012-10-03"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task15": {
+		"checkbox": ["Choose a day", "Friday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "5"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Tests"],
+		"date": ["Due Date:", "2012-10-04"],
+		"comments": ["My Notes:", "To do"]
+	},		
+		"task16": {
+		"checkbox": ["Choose a day", "Monday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "3"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Grade"],
+		"date": ["Due Date:", "2012-10-05"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task17": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Social Studies"],
+		"period": ["Period:", "4"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-10-06"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task18": {
+		"checkbox": ["Choose a day", "Thursday"],
+		"sub": ["Subject:", "Science"],
+		"period": ["Period:", "1"],
+		"grade": ["Grade Level:", "5"],
+		"category": ["Categories:","Contact"],
+		"date": ["Due Date:", "2012-10-07"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task19": {
+		"checkbox": ["Choose a day", "Friday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "3"],
+		"grade": ["Grade Level:", "3"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-10-08"],
+		"comments": ["My Notes:", "To do"]
+	},		
+	"task20": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Reading"],
+		"period": ["Period:", "4"],
+		"grade": ["Grade Level:", "4"],
+		"category": ["Categories:","Lessons"],
+		"date": ["Due Date:", "2012-10-09"],
+		"comments": ["My Notes:", "To do"]
+	},
 }
