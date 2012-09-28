d8e6007acb564e8b205ebeaddde3834e3894358b
Marylia Nieves <marylian@fullsail.edu>
2012-09-27 21:26:00 -0400

    Fixed minor issues.

--- js/json.js
+++ js/json.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-	//Marylia Nieves
-=======
 //Marylia Nieves
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 // MiU September 27, 2012
 //JSON File 
 
@@ -35,11 +31,7 @@ var json = {
 		"date": ["Due Date:", "2012-08-08"],
 		"comments": ["My Notes:", "Teacher Tasks"]
 	},
-<<<<<<< HEAD
 	"task4": {
-=======
-		"task4": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Tuesday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "2"],
@@ -48,11 +40,7 @@ var json = {
 		"date": ["Due Date:", "2012-08-08"],
 		"comments": ["My Notes:", "To do"]
 	},
-<<<<<<< HEAD
 	"task5": {
-=======
-		"task5": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Monday"],
 		"sub": ["Subject:", "Reading"],
 		"period": ["Period:", "1"],
@@ -60,12 +48,8 @@ var json = {
 		"category": ["Categories:","Contact"],
 		"date": ["Due Date:", "2012-09-24"],
 		"comments": ["My Notes:", "To do"]	
-	},	
-<<<<<<< HEAD
+	},
 	"task6": {
-=======
-		"task6": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Tuesday"],
 		"sub": ["Subject:", "Social Studies"],
 		"period": ["Period:", "2"],
@@ -74,11 +58,7 @@ var json = {
 		"date": ["Due Date:", "2012-09-25"],
 		"comments": ["My Notes:", "To do"]	
 	},		
-<<<<<<< HEAD
 	"task7": {
-=======
-		"task7": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Wednesday"],
 		"sub": ["Subject:", "Science"],
 		"period": ["Period:", "3"],
@@ -86,12 +66,8 @@ var json = {
 		"category": ["Categories:","Tests"],
 		"date": ["Due Date:", "2012-09-26"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task8": {
-=======
-		"task8": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Thursday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "4"],
@@ -100,11 +76,7 @@ var json = {
 		"date": ["Due Date:", "2012-09-27"],
 		"comments": ["My Notes:", "To do"]
 	},		
-<<<<<<< HEAD
 	"task9": {
-=======
-		"task9": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Friday"],
 		"sub": ["Subject:", "Writing"],
 		"period": ["Period:", "5"],
@@ -112,12 +84,8 @@ var json = {
 		"category": ["Categories:","Lessons"],
 		"date": ["Due Date:", "2012-09-28"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task10": {
-=======
-		"task10": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Wednesday"],
 		"sub": ["Subject:", "Science"],
 		"period": ["Period:", "1"],
@@ -125,12 +93,8 @@ var json = {
 		"category": ["Categories:","Lessons"],
 		"date": ["Due Date:", "2012-09-29"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task11": {
-=======
-		"task11": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Thursday"],
 		"sub": ["Subject:", "Writing"],
 		"period": ["Period:", "4"],
@@ -138,12 +102,8 @@ var json = {
 		"category": ["Categories:","Lessons"],
 		"date": ["Due Date:", "2012-09-30"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task12": {
-=======
-		"task12": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Tuesday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "2"],
@@ -152,11 +112,7 @@ var json = {
 		"date": ["Due Date:", "2012-10-01"],
 		"comments": ["My Notes:", "To do"]
 	},		
-<<<<<<< HEAD
 	"task13": {
-=======
-		"task13": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Wednesday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "4"],
@@ -164,12 +120,8 @@ var json = {
 		"category": ["Categories:","Lessons"],
 		"date": ["Due Date:", "2012-10-02"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task14": {
-=======
-		"task14": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Thursday"],
 		"sub": ["Subject:", "Reading"],
 		"period": ["Period:", "5"],
@@ -178,11 +130,7 @@ var json = {
 		"date": ["Due Date:", "2012-10-03"],
 		"comments": ["My Notes:", "To do"]
 	},		
-<<<<<<< HEAD
 	"task15": {
-=======
-		"task15": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Friday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "5"],
@@ -199,12 +147,8 @@ var json = {
 		"category": ["Categories:","Grade"],
 		"date": ["Due Date:", "2012-10-05"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task17": {
-=======
-		"task17": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Wednesday"],
 		"sub": ["Subject:", "Social Studies"],
 		"period": ["Period:", "4"],
@@ -213,11 +157,7 @@ var json = {
 		"date": ["Due Date:", "2012-10-06"],
 		"comments": ["My Notes:", "To do"]
 	},		
-<<<<<<< HEAD
 	"task18": {
-=======
-		"task18": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Thursday"],
 		"sub": ["Subject:", "Science"],
 		"period": ["Period:", "1"],
@@ -226,11 +166,7 @@ var json = {
 		"date": ["Due Date:", "2012-10-07"],
 		"comments": ["My Notes:", "To do"]
 	},		
-<<<<<<< HEAD
 	"task19": {
-=======
-		"task19": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Friday"],
 		"sub": ["Subject:", "Math"],
 		"period": ["Period:", "3"],
@@ -238,12 +174,8 @@ var json = {
 		"category": ["Categories:","Lessons"],
 		"date": ["Due Date:", "2012-10-08"],
 		"comments": ["My Notes:", "To do"]
-	},		
-<<<<<<< HEAD
+	},
 	"task20": {
-=======
-		"task20": {
->>>>>>> Changed the background color, made some other minor changes either in html or CSS.
 		"checkbox": ["Choose a day", "Tuesday"],
 		"sub": ["Subject:", "Reading"],
 		"period": ["Period:", "4"],
